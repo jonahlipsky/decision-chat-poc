a68505b34e95e45c4d9f6a53614c0bb0113c7dbf
Jonah Lipsky <jonahlipsky@gmail.com>
2023-12-21 11:25:41 -0500

    add api gateway request response

--- go-lambda-chat/lambda-poc/main.go
+++ go-lambda-chat/lambda-poc/main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"context"
 	"encoding/json"
 	"fmt"
 
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/bedrockruntime"
@@ -35,22 +35,34 @@ type InputPrompt struct {
 	Prompt string `json:"prompt"`
 }
 
-func handler(ctx context.Context, event *InputPrompt) (*string, error) {
+func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	mySession := session.Must(session.NewSession())
 	svc := bedrockruntime.New(mySession)
 
 	modelId := "anthropic.claude-v2"
+	var input InputPrompt
+
+	// NOTE: COME BACK TO THIS FOR SECURITY CONCERNS
+	headers := map[string]string{"Access-Control-Allow-Origin": "*", "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"}
+	multiheaders := map[string][]string{}
+	badrequest := 400
+
+	var errorresp string
+
+	if err := json.Unmarshal([]byte(request.Body), input); err != nil {
+		errorresp = fmt.Sprintf("Unable to unmarshal request body: %s", request.Body)
+		return events.APIGatewayProxyResponse{badrequest, headers, multiheaders, string(errorresp), false}, err
+	}
 
 	body, err := json.Marshal(ClaudeRequest{
-		Prompt:            event.Prompt,
+		Prompt:            input.Prompt,
 		MaxTokensToSample: 4000,
 		Temperature:       0.5,
 	})
 
-	var errorresp string
 	if err != nil {
-		errorresp = fmt.Sprintf("Unable to Marshal request: %s", event.Prompt)
-		return &errorresp, err
+		errorresp = fmt.Sprintf("Unable to Marshal request: %s", input.Prompt)
+		return events.APIGatewayProxyResponse{badrequest, headers, multiheaders, string(errorresp), false}, err
 	}
 
 	req, resp := svc.InvokeModelRequest(&bedrockruntime.InvokeModelInput{
@@ -59,16 +71,16 @@ func handler(ctx context.Context, event *InputPrompt) (*string, error) {
 
 	if err := req.Send(); err != nil {
 		errorresp = "Error sending model invocation."
-		return &errorresp, err
+		return events.APIGatewayProxyResponse{badrequest, headers, multiheaders, string(errorresp), false}, err
 	}
 
 	var response ClaudeResponse
 	if err := json.Unmarshal(resp.Body, &response); err != nil {
 		errorresp = "Unable to Unmarshal ClaudeResponse."
-		return &errorresp, err
+		return events.APIGatewayProxyResponse{badrequest, headers, multiheaders, string(errorresp), false}, err
 	}
 
-	return &response.Completion, nil
+	return events.APIGatewayProxyResponse{200, headers, multiheaders, string(resp.Body), false}, nil
 }
 
 func main() {
