9f2e98922c3259fbc606537e141bece4c7e2282f
Jonah Lipsky <jonahlipsky@gmail.com>
2023-12-22 16:24:03 -0500

    update prompt and testfile

--- go-lambda-chat/lambda-poc/main_test.go
+++ go-lambda-chat/lambda-poc/main_test.go
@@ -1,64 +1,64 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-	"net/http/httptest"
 	"testing"
-
-	"github.com/aws/aws-lambda-go/events"
 )
 
 func TestHandler(t *testing.T) {
-	t.Run("Unable to get IP", func(t *testing.T) {
-		DefaultHTTPGetAddress = "http://127.0.0.1:12345"
+	// t.Run("Unable to get IP", func(t *testing.T) {
+	// 	DefaultHTTPGetAddress = "http://127.0.0.1:12345"
 
-		_, err := handler(events.APIGatewayProxyRequest{})
-		if err == nil {
-			t.Fatal("Error failed to trigger with an invalid request")
-		}
-	})
+	// 	_, err := handler(events.APIGatewayProxyRequest{})
+	// 	if err == nil {
+	// 		t.Fatal("Error failed to trigger with an invalid request")
+	// 	}
+	// })
 
-	t.Run("Non 200 Response", func(t *testing.T) {
-		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			w.WriteHeader(500)
-		}))
-		defer ts.Close()
+	// t.Run("Non 200 Response", func(t *testing.T) {
+	// 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	// 		w.WriteHeader(500)
+	// 	}))
+	// 	defer ts.Close()
 
-		DefaultHTTPGetAddress = ts.URL
+	// 	DefaultHTTPGetAddress = ts.URL
 
-		_, err := handler(events.APIGatewayProxyRequest{})
-		if err != nil && err.Error() != ErrNon200Response.Error() {
-			t.Fatalf("Error failed to trigger with an invalid HTTP response: %v", err)
-		}
-	})
+	// 	_, err := handler(events.APIGatewayProxyRequest{})
+	// 	if err != nil && err.Error() != ErrNon200Response.Error() {
+	// 		t.Fatalf("Error failed to trigger with an invalid HTTP response: %v", err)
+	// 	}
+	// })
 
-	t.Run("Unable decode IP", func(t *testing.T) {
-		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			w.WriteHeader(500)
-		}))
-		defer ts.Close()
+	// t.Run("Unable decode IP", func(t *testing.T) {
+	// 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	// 		w.WriteHeader(500)
+	// 	}))
+	// 	defer ts.Close()
 
-		DefaultHTTPGetAddress = ts.URL
+	// 	DefaultHTTPGetAddress = ts.URL
 
-		_, err := handler(events.APIGatewayProxyRequest{})
-		if err == nil {
-			t.Fatal("Error failed to trigger with an invalid HTTP response")
-		}
-	})
+	// 	_, err := handler(events.APIGatewayProxyRequest{})
+	// 	if err == nil {
+	// 		t.Fatal("Error failed to trigger with an invalid HTTP response")
+	// 	}
+	// })
 
-	t.Run("Successful Request", func(t *testing.T) {
-		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			w.WriteHeader(200)
-			fmt.Fprintf(w, "127.0.0.1")
-		}))
-		defer ts.Close()
+	// t.Run("Successful Request", func(t *testing.T) {
+	// 	_, err := handler(events.APIGatewayProxyRequest{
+	// 		Body: "{ \"prompt\": \"Human: Your role is to provide feedback on a decision that I am trying to make. You can begin.\n  \n  Assistant:\" }",
+	// 	})
+	// 	if err != nil {
+	// 		t.Fatal("Everything should be ok")
+	// 	}
+	// })
 
-		DefaultHTTPGetAddress = ts.URL
-
-		_, err := handler(events.APIGatewayProxyRequest{})
-		if err != nil {
-			t.Fatal("Everything should be ok")
-		}
-	})
+	// t.Run("Marshal and Unmarshal InputPrompt", func(t *testing.T) {
+	// 	input := InputPrompt{
+	// 		Prompt: "Human: Your job... \n\n Assistant:",
+	// 	}
+	// 	encoded, _ := json.Marshal(input)
+	// 	fmt.Print("hello")
+	// 	fmt.Print(encoded)
+	// 	var decoded InputPrompt
+	// 	json.Unmarshal(encoded, &decoded)
+	// })
 }
