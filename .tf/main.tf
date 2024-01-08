b5492d2c19f25fdf1f363ba64ae1e65b4727ba7f
Jonah Lipsky <jonahlipsky@gmail.com>
2024-01-08 18:04:06 -0500

    narrow it down to cors preflight

--- .tf/main.tf
+++ .tf/main.tf
@@ -34,6 +34,27 @@ resource "aws_api_gateway_stage" "development" {
   deployment_id = aws_api_gateway_deployment.initial_chat_api.id
   rest_api_id   = aws_api_gateway_rest_api.chat_poc_api.id
   stage_name    = "development"
+  access_log_settings {
+    destination_arn = aws_cloudwatch_log_group.chatloggroup.arn
+    format = "{ \"requestId\":\"$context.requestId\", \"extendedRequestId\":\"$context.extendedRequestId\", \"ip\": \"$context.identity.sourceIp\", \"caller\":\"$context.identity.caller\", \"user\":\"$context.identity.user\", \"requestTime\":\"$context.requestTime\", \"httpMethod\":\"$context.httpMethod\", \"resourcePath\":\"$context.resourcePath\", \"status\":\"$context.status\", \"protocol\":\"$context.protocol\", \"responseLength\":\"$context.responseLength\" }"
+  }
+}
+
+resource "aws_api_gateway_method_settings" "all" {
+  rest_api_id = aws_api_gateway_rest_api.chat_poc_api.id
+  stage_name  = aws_api_gateway_stage.development.stage_name
+  method_path = "*/*"
+
+  settings {
+    metrics_enabled = true
+    data_trace_enabled = true
+    logging_level   = "INFO"
+  }
+}
+
+resource "aws_cloudwatch_log_group" "chatloggroup" {
+  name              = "API-Gateway-Execution-Logs_${aws_api_gateway_rest_api.chat_poc_api.id}/development"
+  retention_in_days = 1
 }
 
 ### USAGE ###
@@ -70,12 +91,23 @@ resource "aws_api_gateway_usage_plan_key" "main" {
   usage_plan_id = aws_api_gateway_usage_plan.chat.id
 }
 
+resource "aws_lambda_permission" "apigw" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = var.lambda_function_name
+  principal     = "apigateway.amazonaws.com"
+
+  # The /*/* portion grants access from any method on any resource
+  # within the API Gateway "REST API".
+  source_arn = "${aws_api_gateway_rest_api.chat_poc_api.execution_arn}/*/*"
+}
+
 ### POST ###
 
 resource "aws_api_gateway_method" "proxy" {
   rest_api_id = aws_api_gateway_rest_api.chat_poc_api.id
   resource_id = aws_api_gateway_resource.root.id
-  http_method = "POST"
+  http_method = "ANY"
   authorization = "NONE"
   api_key_required = true
 }
@@ -84,16 +116,16 @@ resource "aws_api_gateway_integration" "lambda_integration" {
   rest_api_id = aws_api_gateway_rest_api.chat_poc_api.id
   resource_id = aws_api_gateway_resource.root.id
   http_method = aws_api_gateway_method.proxy.http_method
-  integration_http_method = "POST"
+  integration_http_method = "ANY"
   type = "AWS_PROXY"
-  uri = aws_lambda_function.initial_chat_lambda.invoke_arn
+  uri = var.initial_chat_lambda_invoke_arn
 }
 
 resource "aws_api_gateway_method_response" "proxy" {
   rest_api_id = aws_api_gateway_rest_api.chat_poc_api.id
   resource_id = aws_api_gateway_resource.root.id
   http_method = aws_api_gateway_method.proxy.http_method
-  status_code = "200"
+  status_code = 200
 
   response_parameters = {
     "method.response.header.Access-Control-Allow-Headers" = true,
@@ -133,15 +165,21 @@ resource "aws_api_gateway_integration" "options_integration" {
   rest_api_id = aws_api_gateway_rest_api.chat_poc_api.id
   resource_id = aws_api_gateway_resource.root.id
   http_method = aws_api_gateway_method.options.http_method
-  integration_http_method = "OPTIONS"
   type = "MOCK"
+  request_templates = {
+    "application/json": "{\"statusCode\": 200}"
+  }
+  depends_on = [aws_api_gateway_method.options]
 }
 
 resource "aws_api_gateway_method_response" "options_response" {
   rest_api_id = aws_api_gateway_rest_api.chat_poc_api.id
   resource_id = aws_api_gateway_resource.root.id
   http_method = aws_api_gateway_method.options.http_method
-  status_code = "200"
+  status_code = 200
+  response_models = {
+    "application/json" = "Empty"
+  }
   response_parameters = {
     "method.response.header.Access-Control-Allow-Headers" = true,
     "method.response.header.Access-Control-Allow-Methods" = true,
@@ -153,7 +191,7 @@ resource "aws_api_gateway_integration_response" "options_integration_response" {
   rest_api_id = aws_api_gateway_rest_api.chat_poc_api.id
   resource_id = aws_api_gateway_resource.root.id
   http_method = aws_api_gateway_method.options.http_method
-  status_code = "200"
+  status_code = 200
 
   response_parameters = {
     "method.response.header.Access-Control-Allow-Headers" =  "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
@@ -167,13 +205,39 @@ resource "aws_api_gateway_integration_response" "options_integration_response" {
   ]
 }
 
-### LAMBDA ###
+resource "aws_api_gateway_gateway_response" "response_4xx" {
+  rest_api_id   = aws_api_gateway_rest_api.chat_poc_api.id
+  response_type = "DEFAULT_4XX"
 
-resource "aws_lambda_function" "initial_chat_lambda" {
-  filename = var.initial_chat_lambda_zipfile_path
-  function_name = "initialChat"
-  role = var.lambda_bedrock_dev_role_arn
-  handler = "bootstrap"
-  runtime = "provided.al2023"
-  timeout = var.lambda_bedrock_timeout
+  response_templates = {
+    "application/json" = "{'message':$context.error.messageString}"
+  }
+
+  response_parameters = {
+    "gatewayresponse.header.Access-Control-Allow-Origin" = "'*'" # replace with hostname of frontend (CloudFront)
+  }
 }
+
+resource "aws_api_gateway_gateway_response" "response_5xx" {
+  rest_api_id   = aws_api_gateway_rest_api.chat_poc_api.id
+  response_type = "DEFAULT_5XX"
+
+  response_templates = {
+    "application/json" = "{'message':$context.error.messageString}"
+  }
+
+  response_parameters = {
+    "gatewayresponse.header.Access-Control-Allow-Origin" = "'*'" # replace with hostname of frontend (CloudFront)
+  }
+}
+
+# LAMBDA
+
+# resource "aws_lambda_function" "initial_chat_lambda" {
+#   filename = var.initial_chat_lambda_zipfile_path
+#   function_name = "initialChat"
+#   role = var.lambda_bedrock_dev_role_arn
+#   handler = "bootstrap"
+#   runtime = "provided.al2023"
+#   timeout = var.lambda_bedrock_timeout
+# }
--- .tf/variables.tf
+++ .tf/variables.tf
@@ -1,17 +1,29 @@
-variable "initial_chat_lambda_zipfile_path" {
-  description = "Name of zipped lambda file for initial chat"
+# variable "initial_chat_lambda_zipfile_path" {
+#   description = "Name of zipped lambda file for initial chat"
+#   type = string
+#   default = "../go-lambda-chat/lambda-poc/chatPocFunction-v0.0.5.zip"
+# }
+
+# variable "lambda_bedrock_dev_role_arn" {
+#   description = "Role to assume for Lambdas invoking Bedrock"
+#   type = string
+#   default = "arn:aws:iam::442848236251:role/chat-bedrock-poc"
+# }
+
+# variable "lambda_bedrock_timeout" {
+#   description = "How long before timeout for Bedrock invocations"
+#   type = number
+#   default = 60
+# }
+
+variable "lambda_function_name" {
+  description = "Name of lambda function"
   type = string
-  default = "../go-lambda-chat/lambda-poc/chatPocFunction-v0.0.5.zip"
+  default = "chatPocFunction"
 }
 
-variable "lambda_bedrock_dev_role_arn" {
-  description = "Role to assume for Lambdas invoking Bedrock"
+variable "initial_chat_lambda_invoke_arn" {
+  description = "ARN for lambda to invoke"
   type = string
-  default = "arn:aws:iam::442848236251:role/chat-bedrock-poc"
-}
-
-variable "lambda_bedrock_timeout" {
-  description = "How long before timeout for Bedrock invocations"
-  type = number
-  default = 60
+  default = "arn:aws:apigateway:us-east-1:lambda:path/2015-03-31/functions/arn:aws:lambda:us-east-1:442848236251:function:chatPocFunction/invocations"
 }
